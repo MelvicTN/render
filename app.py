46267f877ad31062c438f7ab93fa1065149ad2a1
MelvicTN <mtnavida@gmail.com>
2024-07-21 14:26:35 -0700

    added code for st.slider and st.button to app.py file

--- app.py
+++ app.py
@@ -2,4 +2,15 @@ import streamlit as st
 
 st.header('Tossing a Coin')
 
+# adding slider and the button to the web app
+# reference the following sites for python code
+# `st.slider` - https://docs.streamlit.io/develop/api-reference/widgets/st.slider
+# `st.button` - https://docs.streamlit.io/develop/api-reference/widgets/st.button
+
+number_of_trials = st.slider('Number of trials?', 1, 1000, 10)
+start_button = st.button('Run')
+
+if start_button:
+    st.write(f'Running the experient of {number_of_trials} trials.')
+
 st.write('It is not a functional application yet. Under construction.')
