18b5c3ac9f664f721ec8ef7762a8e552ba13958d
MelvicTN <mtnavida@gmail.com>
2024-07-21 14:57:21 -0700

    added chart variable + toss_coin function + calculates mean + start button call

--- app.py
+++ app.py
@@ -1,8 +1,37 @@
+import scipy.stats
 import streamlit as st
+import time
 
 st.header('Tossing a Coin')
 
-# adding slider and the button to the web app
+
+# add line plot chart
+chart = st.line_chart([0.5])
+
+
+# toss_coin function defined that emulates tossing a coin `n` times
+# calculates the mean with every new iteration which adds to `chart`(as a new observation)
+
+def toss_coin(n):
+
+    trial_outcomes = scipy.stats.bernoulli.rvs(p=0.5, size=n)
+
+    mean = None
+    outcome_no = 0
+    outcome_1_count = 0
+
+    for r in trial_outcomes:
+        outcome_no +=1
+        if r == 1:
+            outcome_1_count += 1
+        mean = outcome_1_count / outcome_no
+        chart.add_rows([mean])
+        time.sleep(0.05)
+
+    return mean
+
+
+# adding slider and the start button to the web app
 # reference the following sites for python code
 # `st.slider` - https://docs.streamlit.io/develop/api-reference/widgets/st.slider
 # `st.button` - https://docs.streamlit.io/develop/api-reference/widgets/st.button
@@ -12,5 +41,4 @@ start_button = st.button('Run')
 
 if start_button:
     st.write(f'Running the experient of {number_of_trials} trials.')
-
-st.write('It is not a functional application yet. Under construction.')
+    mean = toss_coin(number_of_trials)
